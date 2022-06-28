7479f24c2410c57b93b54e5bb1e68759178c7556
Ryan Turner <RyanTurner02@protonmail.com>
2022-06-28 00:26:00 -0400

    Add fields and getter methods to webpage class

--- src/main/java/Webpage.java
+++ src/main/java/Webpage.java
@@ -1,3 +1,19 @@
-public class Webpage {
+import org.jsoup.nodes.Document;
 
+public class Webpage {
+    String title;
+    String URL;
+    Document document;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
 }
