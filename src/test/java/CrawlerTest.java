79b2cb0930f87392cbf0701a30ac3c413fa3e78f
Ryan Turner <RyanTurner02@protonmail.com>
2022-06-30 23:17:36 -0400

    Create a test for link with parentheses

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -1,18 +1,32 @@
-import jdk.jfr.Name;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CrawlerTest {
-    final String TESTING_URL = "https://en.wikipedia.org/wiki/Wikipedia";
-
     @Test
-    @Name("First Link Is Valid 1")
+    @DisplayName("First Link is Valid without Parentheses")
     void firstLinkIsValid1() {
-        Webpage webpage = new Webpage(TESTING_URL);
+        String testingURL = "https://en.wikipedia.org/wiki/Wikipedia";
+
+        Webpage webpage = new Webpage(testingURL);
         Crawler webCrawler = new Crawler(webpage);
 
         String expected = "https://en.wikipedia.org/wiki/Multilingualism";
+        String actual = "https://en.wikipedia.org" + webCrawler.getFirstValidLink();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("First Link is Valid with Parentheses")
+    void firstLinkIsValid2() {
+        String testingURL = "https://en.wikipedia.org/wiki/Point_and_click";
+
+        Webpage webpage = new Webpage(testingURL);
+        Crawler webCrawler = new Crawler(webpage);
+
+        String expected = "https://en.wikipedia.org/wiki/User_(computing)";
         String actual = "https://en.wikipedia.org" + webCrawler.getFirstValidLink();
 
         assertEquals(expected, actual);
