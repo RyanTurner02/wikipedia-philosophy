ff2a74d3f35fa67d5950ac667729d6073b347230
Ryan Turner <RyanTurner02@protonmail.com>
2022-06-30 20:12:31 -0400

    Print the header of the initial page

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,16 @@ public class Main {
         Webpage initialWebpage = new Webpage(URL);
 
         Crawler webCrawler = new Crawler(initialWebpage);
-        String heading = "";
+        String heading = webCrawler.getFirstHeading();
+        System.out.println(heading);
 
         while (!heading.equals("Philosophy")) {
             String nextLink = "https://en.wikipedia.org" + webCrawler.getFirstValidLink();
             Webpage nextWebpage = new Webpage(nextLink);
+
             webCrawler = new Crawler(nextWebpage);
             heading = webCrawler.getFirstHeading();
+
             System.out.println(heading);
         }
     }
